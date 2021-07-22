5341aa63e6e7bd310541c9ba5609443aeead0996
Gustavo251199 <gus-tavo99@outlook.es>
2021-07-22 02:41:35 -0500

    dash

--- src/controller/kardex.controller.js
+++ src/controller/kardex.controller.js
@@ -3,9 +3,13 @@ const { cns } = require('../config/index');
 
 const getKardex = async (req, res) => {
   const conn = await oracledb.getConnection(cns);
-  const result = await conn.execute('SELECT * FROM KARDEX', [], {
-    outFormat: oracledb.OUT_FORMAT_OBJECT,
-  });
+  const result = await conn.execute(
+    'SELECT ID, PRODUCTO, TRUNC(FECHA), ACCION FROM KARDEX',
+    [],
+    {
+      outFormat: oracledb.OUT_FORMAT_OBJECT,
+    }
+  );
   res.status(200).json(result.rows);
 };
 
